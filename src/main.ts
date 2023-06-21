6ad161540c8dd6536715e5a795a5c8f7da55cbee
Veirt <vveirt@gmail.com>
2023-06-21 22:12:12 +0800

    add cron and handle errors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import * as cheerio from "cheerio";
 import dotenv from "dotenv";
 import fs from "fs";
+import * as cron from "node-cron";
+import pRetry, { AbortError } from "p-retry";
 import puppeteer from "puppeteer";
 import { createHttpServer } from "./server.js";
 
@@ -10,27 +12,31 @@ dotenv.config();
 const LOGIN_PAGE_URL = "https://sia.unmul.ac.id/login";
 
 async function getLoginCookie() {
-    if (process.env.NIM === undefined || process.env.PASSWORD === undefined) {
-        console.error("Please set environment variable NIM and PASSWORD first.");
-        process.exit();
-    }
+  if (process.env.NIM === undefined || process.env.PASSWORD === undefined) {
+    console.error("Please set environment variable NIM and PASSWORD first.");
+    process.exit();
+  }
 
-    const browser = await puppeteer.launch({ headless: "new" });
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+      product: "firefox",
+    });
     const page = await browser.newPage();
     page.goto(LOGIN_PAGE_URL);
 
     const captchaSelector = "div.form-group:nth-child(3) > div:nth-child(1)";
     const inputSelector = {
-        email: "#exampleInputEmail",
-        password: "div.form-group:nth-child(2) > input:nth-child(1)",
-        captcha: "div.form-group:nth-child(4) > input:nth-child(1)",
-        submit: "button.btn",
+      email: "#exampleInputEmail",
+      password: "div.form-group:nth-child(2) > input:nth-child(1)",
+      captcha: "div.form-group:nth-child(4) > input:nth-child(1)",
+      submit: "button.btn",
     };
     await page.waitForSelector(captchaSelector);
 
     const captcha = await page.$eval(
-        captchaSelector,
-        (element) => element.innerHTML
+      captchaSelector,
+      (element) => element.innerHTML
     );
     await page.type(inputSelector.email, process.env.NIM);
     await page.type(inputSelector.password, process.env.PASSWORD);
@@ -41,58 +47,91 @@ async function getLoginCookie() {
     await browser.close();
 
     const ci_session_cookie = cookies.filter(
-        (cookie) => cookie.name === "ci_session"
+      (cookie) => cookie.name === "ci_session"
     )[0];
 
     fs.writeFileSync(
-        "cookie.txt",
-        `ci_session=${ci_session_cookie.value}`,
-        "utf8"
+      "cookie.txt",
+      `ci_session=${ci_session_cookie.value}`,
+      "utf8"
     );
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-const cookieExist = fs.existsSync("cookie.txt");
-if (!cookieExist) {
-    await getLoginCookie();
-}
-
-const cookie = fs.readFileSync("cookie.txt", "utf8").trim();
-
-async function getTranscript() {
+async function fetchTranscript(cookie: string) {
+  try {
     const response = await axios.get(
-        "https://sia.unmul.ac.id/mhstranskrip/loaddatas",
-        {
-            headers: { cookie },
-        }
+      "https://sia.unmul.ac.id/mhstranskrip/loaddatas",
+      {
+        headers: { cookie },
+      }
     );
 
     return response;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      if (err.response?.status) {
+        throw new AbortError(`Status code error: ${err.response.statusText}`);
+      }
+
+      throw new Error(err.message);
+    }
+  }
 }
 
-let response = await getTranscript();
+async function saveTranscript() {
+  const cookieExist = fs.existsSync("cookie.txt");
+  if (!cookieExist) {
+    await pRetry(getLoginCookie, { retries: 3 });
+  }
 
-if (response.headers.refresh === "0;url=https://sia.unmul.ac.id/login") {
-    console.log("Getting cookies and retrying request...");
-    await getLoginCookie();
+  const cookie = fs.readFileSync("cookie.txt", "utf8").trim();
 
-    response = await getTranscript();
-}
+  let response;
+  try {
+    response = await pRetry(() => fetchTranscript(cookie), { retries: 3 });
+    if (!response) {
+      throw new Error("Response is empty.");
+    }
 
-const transcriptHtmlExist = fs.existsSync("data/transcript.html");
-if (transcriptHtmlExist) {
+    if (response.headers.refresh === "0;url=https://sia.unmul.ac.id/login") {
+      console.log("Getting cookies and retrying request...");
+      await pRetry(getLoginCookie, { retries: 3 });
+
+      response = await fetchTranscript(cookie);
+    }
+  } catch (err) {
+    console.error("Failed fetching transcript.");
+    console.error(err);
+    return;
+  }
+
+  const transcriptHtmlExist = fs.existsSync("data/transcript.html");
+  if (transcriptHtmlExist) {
     const oldTranscript = fs.readFileSync("data/transcript.html", "utf8");
-    const newTranscript = response.data;
+    const newTranscript = response!.data;
 
     if (oldTranscript !== newTranscript) {
-        fs.writeFileSync("data/transcript.html", response.data, "utf8");
+      console.log("Something changed.");
+      fs.writeFileSync("data/transcript.html", response!.data, "utf8");
     }
-} else {
-    fs.writeFileSync("data/transcript.html", response.data, "utf8");
+  } else {
+    fs.writeFileSync("data/transcript.html", response!.data, "utf8");
+  }
+
+  const transcriptHtml = fs.readFileSync("data/transcript.html");
+  const $ = cheerio.load(transcriptHtml);
+  $(".row").remove();
+  fs.writeFileSync("data/transcript-render.html", $.html(), "utf8");
 }
 
-const transcriptHtml = fs.readFileSync("data/transcript.html");
-const $ = cheerio.load(transcriptHtml);
-$(".row").remove();
-fs.writeFileSync("data/transcript-render.html", $.html(), "utf8");
+// run at the beginning
+await saveTranscript();
+cron.schedule("*/30 6-23 * * *", async () => {
+  console.log(`[${new Date().toLocaleString()}] Running CRON job.`);
+  await saveTranscript();
+});
 
 createHttpServer();
